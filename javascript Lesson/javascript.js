a54eb43d6f9ce87eec35927b1bcae40fda1e1dd2
Oluwatobi-makee <tobi58052@gmail.com>
2022-11-04 12:37:16 +0100

    Initial commit

---
+++ javascript Lesson/javascript.js
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>what can javascript Do?</h1>
+    <p id="demo"> javascript can change HTML content.</p>
+    <button type = "button"
+    onclick="document.getElementById('demo').inner HTML='Hello Javascript!'">click Me!</button>
+</body>
